c7bbf870ed8c9ad27171b543eae7745135f76a3b
Melih Boyacı <gsmelihboyaci@gmail.com>
2025-07-09 10:01:49 +0300

    Enhance logging for new user connections to include username if available

--- main.go
+++ main.go
@@ -234,7 +234,12 @@ func (h *Hub) run() {
 			h.mutex.Lock()
 			h.clients[client] = true
 			h.mutex.Unlock()
-			log.Printf("Yeni kullanıcı bağlandı. ID: %s", client.ID)
+			// Kullanıcı adı varsa logda göster
+			if client.Username != "" {
+				log.Printf("Yeni kullanıcı bağlandı. ID: %s, Kullanıcı: %s", client.ID, client.Username)
+			} else {
+				log.Printf("Yeni kullanıcı bağlandı. ID: %s", client.ID)
+			}
 
 			// Broadcast updated user count
 			go h.broadcastUserCount()
